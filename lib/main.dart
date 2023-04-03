6268ae98fd6f83700650864127433c99888a15a2
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-04-03 20:23:39 -0300

    Criando o componente inicial.

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,61 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProjetoFlutterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ProjetoFlutterState extends State<ProjetoFlutterApp> {
+  var indexPergunta = 0;
+
+  void responder() {
+    setState(() {
+      indexPergunta++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual a sua cor favorita?',
+      'Qual o nome do seu cachorro?',
+      'Qual o seu anime favorito?'
+    ];
+
     return MaterialApp(
-      title: 'Projeto Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Projeto Flutter'),
           centerTitle: true,
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
+          children: [
+            Text(perguntas[indexPergunta]),
+            ElevatedButton(
+                onPressed: responder,
+                child: const Text('Resposta 1')
+            ),
+            ElevatedButton(
+                onPressed: responder,
+                child: const Text('Resposta 2')
+            ),
+            ElevatedButton(
+                onPressed: responder,
+                child: const Text('Resposta 3')
+            )
           ],
         ),
       ),
     );
   }
+}
+
+class ProjetoFlutterApp extends StatefulWidget {
+
+  const ProjetoFlutterApp({super.key});
+
+  @override
+  ProjetoFlutterState createState() {
+    return ProjetoFlutterState();
+  }
+
 }
